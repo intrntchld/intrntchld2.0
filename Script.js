bbd1df2844e7db50359b765ecf135492b45410e3
intrntchld <152072113+intrntchld@users.noreply.github.com>
2023-12-02 06:03:29 -0700

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,14 @@
+// Example JavaScript code
+console.log('Hello, GitHub!');
+<!-- index.html -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>JavaScript Example</title>
+</head>
+<body>
+  <script src="script.js"></script>
+</body>
+</html>
